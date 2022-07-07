ad2ce63f430b26ff4e78faebd6ec224c5817211a
Sima612 <sima101531@yahoo.com>
2022-07-07 09:48:14 -0700

    changed code

--- functions_practice.py
+++ functions_practice.py
@@ -8,11 +8,13 @@ def pack(arg1, arg2, arg3):
 
 print(pack('Pack 1', 'Pack 2', 'Pack 3'))
 
-
-def eat_lunch(list1, list2):
-  if list1 == 0:
+def eat_lunch(list1):
+  if len(list1) == 0:
     print('My Lunchbox is empty!')
-  elif list1 != 0:
-    print('First I eat ' + list1 + ', and ' + 'next I eat ' + list2 + '!')
+  for i in range(len(list1)):
+    if i == 0:
+      print(f'First I eat {list1[0]}')
+    else:
+      print(f'Next I eat {list1[i]}')
 
-eat_lunch('Candies', 'Chocolates')
+eat_lunch(['candies', 'chocolates'])
